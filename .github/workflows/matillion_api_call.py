94bf540999364c3d2b6e0f84bd269cd15a8aba68
bikramjot <129281744+bikramjot@users.noreply.github.com>
2023-11-08 13:57:15 +0530

    Update matillion_api_call.py

--- .github/workflows/matillion_api_call.py
+++ .github/workflows/matillion_api_call.py
@@ -64,7 +64,7 @@ if azrepo_status['success']:
     }
     print("CommitId:-", commitid)
     
-    '''
+    
     x = requests.post(url_switch, json=body_commit, auth=(str(user), str(password)), verify=False)
     print("switch")
     res = json.loads(x.text)
@@ -75,7 +75,7 @@ if azrepo_status['success']:
     else:
         print("switched failed")
         exit(1)
-    '''
+    
     
     exit(0)
 
